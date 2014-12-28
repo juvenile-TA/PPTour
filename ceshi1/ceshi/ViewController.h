3e00f2de137824786ffcea1a42b321deff48a774
juvenile-TA <ylibang@hotmail.com>
2014-12-29 00:02:49 +0800

    添加测试1

---
+++ ceshi1/ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by 李棒 on 14-11-21.
+//  Copyright (c) 2014年 李棒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ceshi1/ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ceshi
+//
+//  Created by 李棒 on 14-11-21.
+//  Copyright (c) 2014年 李棒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
