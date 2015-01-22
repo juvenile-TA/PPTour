5a8cd88197600fd5942beb7909e1b0820c49197d
juvenile-TA <ylibang@hotmail.com>
2015-01-22 23:05:23 +0800

    ---

--- MasonryDemo/MasonryDemo/ViewController.h
+++ MasonryDemo/MasonryDemo/ViewController.h
@@ -14,9 +14,18 @@
 @property (nonatomic ,strong) NSString* shuahuoyued ;
 @property (nonatomic ,strong) NSString* shuahuoyueu ;
 //sadfdsdsgsda
-@property (nonatomic ,strong) NSString* shuahuoyuedu ;
-@property (nonatomic ,strong) NSString* shuahuoyued ;
-@property (nonatomic ,strong) NSString* shuahuoyueu ;
+@property (nonatomic ,strong) NSString* huahuoyuedu ;
+@property (nonatomic ,strong) NSString* shuahuoyue ;
+@property (nonatomic ,strong) NSString* shuahuoyu ;
+@property (nonatomic ,strong) NSString* shuahuoyuu ;
+@property (nonatomic ,strong) NSString* shuahuoyud ;
+@property (nonatomic ,strong) NSString* shuahuoyeu ;
+@property (nonatomic ,strong) NSString* shuahuouedu ;
+@property (nonatomic ,strong) NSString* shuahuyued ;
+@property (nonatomic ,strong) NSString* shuahoyueu ;
+@property (nonatomic ,strong) NSString* shuauoyuedu ;
+@property (nonatomic ,strong) NSString* shuhuoyued ;
+@property (nonatomic ,strong) NSString* shahuoyueu ;
 
 @end
 
